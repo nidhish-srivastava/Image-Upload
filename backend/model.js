2934dd03b4c9e61e4cb91b867e3ee681e9e6af51
Nidhish Srivastava <nidhish0430@gmail.com>
2023-07-14 21:50:09 +0530

    ..

--- backend/model.js
+++ backend/model.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const userProfileSchema = new mongoose.Schema({
     dp : String,
-    username : String
 }); 
 
 module.exports = mongoose.model("UserProfile", userProfileSchema);
